d48484c3481e727dc773432a5b7a1afef9c5ad96
Al Bilal <bil.bank@yahoo.com>
2022-01-01 17:56:04 -0500

    assignment completed' .exit cd ..

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
+element.textContent = "You've changed what's on the screen!";
+
+element.style.fontSize = "24px";
+element.style.marginLeft = "30px";
+element.style.lineHeight = 2;
+
+const newHeader = document.createElement (`h1`)
+newHeader.id = (`victory`)
+newHeader.textContent = `Bil is the champion`
+
+
+const main = document.getElementById ("main");
+main.remove()
